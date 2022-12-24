58cbba4697cb1392311b498c9b8b8624112c168f
laviyre <54475683+laviyre@users.noreply.github.com>
2022-12-24 13:58:21 +1100

    Add Insertion Sort

--- src/sorting-algorithms/algorithms/index.tsx
+++ src/sorting-algorithms/algorithms/index.tsx
@@ -1,10 +1,12 @@
 import BubbleSort from "./bubble-sort";
+import InsertionSort from "./insertion-sort";
 import SelectionSort from "./selection-sort";
 import SortingAlgorithm from "./sorting-algorithm";
 
 const sortingAlgorithms: Array<SortingAlgorithm> = [
     new BubbleSort(),
     new SelectionSort(),
+    new InsertionSort(),
 ];
 
 export default sortingAlgorithms;
---
+++ src/sorting-algorithms/algorithms/insertion-sort.ts
@@ -0,0 +1,17 @@
+import SortingAlgorithm from "./sorting-algorithm";
+import { SortableArray } from "../sortable-array";
+
+class InsertionSort implements SortingAlgorithm {
+    sort(arr: SortableArray): Array<any> {
+        for (let i = 0; i < arr.getValues().length; i++) {
+            for (let j = i; j > 0; j--) {
+                if (arr.compare(j, j-1))
+                    arr.swap(j, j-1);
+            }
+        }
+
+        return arr.getValues();
+    }
+}
+
+export default InsertionSort;
