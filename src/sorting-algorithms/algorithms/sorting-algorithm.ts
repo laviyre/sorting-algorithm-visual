622cb19f4bffeb7c4f904d4b89e9855bd7e134d7
laviyre <54475683+laviyre@users.noreply.github.com>
2023-02-06 22:17:50 +1100

    Remove src to fix case bug

--- src/sorting-algorithms/algorithms/sorting-algorithm.ts
+++
@@ -1,8 +0,0 @@
-import {SortableArray} from "../sortable-array";
-
-interface SortingAlgorithmAsync {
-    sort(values: SortableArray): Promise<Array<any>>;
-    getName(): string;
-}
-
-export type {SortingAlgorithmAsync};
